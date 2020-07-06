2687e33c0db4f24727dc57165a367b90cfb94701
searleser97 <gabrielsanv97@gmail.com>
2020-07-06 16:17:54 -0500

    updated on 07/06/2020 at 04:17:54 PM

--- src/Tester.ts
+++ src/Tester.ts
@@ -3,14 +3,21 @@ import Config from "./Config";
 export default class Tester {
     config: Config;
     filePath: string;
+    fileExtension: string | undefined;
+    filePathNoExtension: string;
     testId?: number;
+
     constructor(config: Config, filePath: string, testId?: number) {
         this.config = config;
         this.filePath = filePath;
         this.testId = testId;
+        let segmentedPath = filePath.split('.');
+        this.filePathNoExtension = segmentedPath[0];
+        this.fileExtension = segmentedPath.pop(); 
     }
 
     run() {
+        // compilation
         if (this.testId) {
         } else {
         }
