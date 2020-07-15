15d4aad7508d9844cf48a375b99145f6d9b7fdfa
searleser97 <gabrielsanv97@gmail.com>
2020-07-15 00:05:10 -0500

    refactor to miliseconds

--- src/SourceFileCreator.ts
+++ src/SourceFileCreator.ts
@@ -21,9 +21,9 @@ import * as Path from "path";
 import Util from "./Util";
 
 export default class SourceFileCreator {
-    static create(filePath: string, config: Config, timeLimitInSeconds: number) {
+    static create(filePath: string, config: Config, timeLimitInMS: number) {
         let extension = Path.extname(filePath);
-        let template = `${Util.getCommentString(extension)} time-limit: ${timeLimitInSeconds}\n`;
+        let template = `${Util.getCommentString(extension)} time-limit: ${timeLimitInMS}\n`;
         if (extension == ".cpp" && config.cppTemplatePath) {
             template += fs.readFileSync(config.cppTemplatePath).toString();
         } else if (extension == ".py" && config.pyTemplatePath) {
