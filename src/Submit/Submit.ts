720bc471fcd70cc969f4732649f8f6aceff5226e
searleser97 <gabrielsanv97@gmail.com>
2021-03-10 18:38:40 -0600

    Add extract url from file function

--- src/Submit/Submit.ts
+++ src/Submit/Submit.ts
@@ -16,10 +16,13 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import { exit } from "process";
 import Config from "../Config/Config";
 import ICommandGlobalArgs from "../Types/ICommandGlobalArgs";
 import OnlineJudgeFactory from "./OnlineJudgeFactory/OnlineJudgeFactory";
+import * as fs from "fs";
 import * as Path from "path";
+import Util from "../Util";
 
 export interface ICommandSubmitArgs extends ICommandGlobalArgs {
   filePath: string;
@@ -27,14 +30,29 @@ export interface ICommandSubmitArgs extends ICommandGlobalArgs {
   langAlias?: string;
 }
 
-function extractUrlFromFile(filePath: string): string {
-  // send error message if url not found in file
-  // return "https://codeforces.com/contest/1486/problem/D";
-  return "https://atcoder.jp/contests/abc194/tasks/abc194_a";
+function extractUrlFromFile(filePath: string): string | undefined {
+  const text = fs.readFileSync(filePath).toString();
+  const match = /problem-url\s*:\s*.+/g.exec(text);
+  if (match) {
+    const patternFound = match[0];
+    let url = patternFound.substr(patternFound.indexOf(":") + 1).trim();
+    return url;
+  } else {
+    return undefined;
+  }
 }
 
 export function submit(args: ICommandSubmitArgs) {
   const url = args.url ?? extractUrlFromFile(args.filePath);
+  if (!url) {
+    const commentString = Util.getCommentString(Path.extname(args.filePath));
+    console.log(
+      "Problem URL couldn't be found in file, please provide it as argument or" +
+        " add it as a comment in your file in the following format:\n\n" +
+        `${commentString} problem-url: <url>`
+    );
+    exit(0);
+  }
   const oj = OnlineJudgeFactory.getOnlineJudge(url);
   const config = Config.read(args.config);
   oj.submit(args.filePath, url, config, args.langAlias);
