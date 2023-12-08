2569bcc8fcc1d7c81eb97f820feae9e5b79e6529
leonardodelfinodev <l.delfino1@studenti.unipi.it>
2023-12-08 14:58:02 +0100

    Handled multiple file opening with vscode

--- app/src/Create/BuildLaunchCommand.ts
+++ app/src/Create/BuildLaunchCommand.ts
@@ -1,5 +1,37 @@
 import * as Path from "path";
 
+function expandRange(filePath: string): string[] {
+    const directory = Path.dirname(filePath);
+    const fullFilename = Path.basename(filePath);
+    const extension = Path.extname(fullFilename);
+    const filenameWithoutExtension = Path.basename(fullFilename, extension);
+
+    const rangePattern = /\{([a-zA-Z])\.\.([a-zA-Z])\}/;
+    const rangeMatch = filenameWithoutExtension.match(rangePattern);
+
+    if (!rangeMatch) return [filePath];
+
+    const [ , startChar, endChar ] = rangeMatch;
+    let expandedPaths = [];
+    let startCode = startChar.toLowerCase().charCodeAt(0);
+    let endCode = endChar.toLowerCase().charCodeAt(0);
+
+    if (endCode < startCode) {
+      [startCode, endCode] = [endCode, startCode];
+    }
+
+    for (let i = startCode; i <= endCode; i++) {
+        expandedPaths.push(
+        	Path.join(directory, 
+				filenameWithoutExtension.replace(
+					rangePattern, String.fromCharCode(i)) + extension
+			)
+		);
+    }
+
+    return expandedPaths;
+}
+
 export function buildLaunchCommand(terminalName: string, filePath: string): string | null {
   const fileDirectory = Path.dirname(filePath);
   switch (terminalName) {
@@ -20,8 +52,10 @@ export function buildLaunchCommand(terminalName: string, filePath: string): stri
 	  return `kitty --directory "${fileDirectory}"`;
 
 	// Editors
-	case "vscode":
-	  return `code "${filePath}"`;
+	case "code":
+      return filePath.includes('{')
+        ? `code ${expandRange(filePath).map(fp => `"${fp}"`).join(' ')}`
+        : `code "${filePath}"`;
 
     default:
       return `${terminalName} "${filePath}"`;
