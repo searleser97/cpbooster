8beb23964ff844e872d471295a99b4ba1dad5c0e
searleser97 <gabrielsanv97@gmail.com>
2025-02-21 15:11:19 -0800

    add google adsense code

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -53,5 +53,15 @@ module.exports = {
         }
       }
     ]
+  ],
+  headTags: [
+    {
+      tagName: "script",
+      attributes: {
+        async: "1",
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5705416960449352",
+        crossorigin: "anonymous"
+      }
+    }
   ]
 };
